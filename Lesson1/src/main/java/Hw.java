a3c0afdebdcd18aab7d45f8277e935ae32787883
Nikolay <alda1995yurevich@gmail.com>
2022-12-09 04:38:03 +0300

    Branches

---
+++ Lesson1/src/main/java/Hw.java
@@ -0,0 +1,6 @@
+public class Hw {
+    public static void main(String[] args) {
+        System.out.println("Idem spat Hello fucking world!");
+        System.out.println("Do you you do ia voidy");
+    }
+}
