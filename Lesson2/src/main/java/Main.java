b232e0bc3840ed9d901acad1759df66c5560ed8c
Nikolay <alda1995yurevich@gmail.com>
2023-01-23 02:25:51 +0300

    homework

---
+++ Lesson2/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        if (true){
+            System.out.println();
+        }
+        System.out.println("addada");
+        System.out.println("aakdkda!");
+    }
+}
