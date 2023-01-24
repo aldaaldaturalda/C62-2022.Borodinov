30909fe8151395121046e750cbd17a72c60bffb0
Nikolay <alda1995yurevich@gmail.com>
2023-01-24 22:26:26 +0300

    homework

--- Lesson1/src/main/java/Hw.java
+++
@@ -1,6 +0,0 @@
-public class Hw {
-    public static void main(String[] args) {
-        System.out.println("Idem spat Hello fucking world!");
-        System.out.println("Do you you do ia voidy");
-    }
-}
