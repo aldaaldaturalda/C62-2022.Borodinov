206f2faa27495670716dd22d71a1d5a2892f0e92
Nikolay <alda1995yurevich@gmail.com>
2023-02-12 00:29:44 +0300

    hw

---
+++ Lesson5/src/main/java/AmebaDivision.java
@@ -0,0 +1,12 @@
+public class AmebaDivision {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 24; i += 3) {
+            System.out.println(calculateAmebaDivision(1, i) + "");
+
+        }
+    }
+
+    private static int calculateAmebaDivision(int begin, int time) {
+        return (int) (begin * Math.pow(2, time / 3));
+    }
+}
---
+++ Lesson5/src/main/java/MassiveOddNumbers.java
@@ -0,0 +1,19 @@
+public class MassiveOddNumbers {
+    //7) Создайте массив из всех нечётных чисел от 1 до 100, выведите его на экран в строку,
+    //        а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 ... 7 5 3 1)
+    public static void main(String[] args) {
+        calculateNumbers();
+    }
+
+    public static void calculateNumbers() {
+        int[] mass = new int[50];
+        for (int i = 0, j = 0; i < 100; j++, i += 2) {
+            mass[j] = i;
+            System.out.println(mass[j] + " ");
+        }
+        System.out.println("!!!");
+        for (int i = mass.length - 1; i >= 0; i--) {
+            System.out.println(mass[i] + " ");
+        }
+    }
+}
