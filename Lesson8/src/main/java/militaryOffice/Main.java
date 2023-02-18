8692f6528f2bb38a25c537f81d05446f9c99a931
Nikolay <alda1995yurevich@gmail.com>
2023-02-19 01:16:18 +0300

    MilitaryOffice hw

--- Lesson8/src/main/java/militaryOffice/Main.java
+++ Lesson8/src/main/java/militaryOffice/Main.java
@@ -1,4 +1,31 @@
 package militaryOffice;
 
+import java.util.Arrays;
+
+import static militaryOffice.Sex.MALE;
+
 public class Main {
+    public static void main(String[] args) {
+        Person[] persons = new Person[15];
+        persons[0] = new Person("Vitaliy", 20, MALE, "Belarus", "Minsk");
+        persons[1] = new Person("Igor", 25, MALE, "Belarus", "Minsk");
+        persons[2] = new Person("Vladimir", 18, MALE, "Belarus", "Minsk");
+        persons[3] = new Person("Alexander", 40, MALE, "Belarus", "Minsk");
+        persons[4] = new Person("Ignat", 27, MALE, "Belarus", "Minsk");
+        persons[5] = new Person("Oleg", 23, MALE, "Belarus", "Homel");
+        persons[6] = new Person("Vitold", 42, MALE, "Belarus", "Minsk");
+        persons[7] = new Person("Maksim", 38, MALE, "Belarus", "Minsk");
+        persons[8] = new Person("Ramil", 21, MALE, "Belarus", "Minsk");
+        persons[9] = new Person("Evgeniy", 29, MALE, "Belarus", "Bobryisk");
+        persons[10] = new Person("Alexander", 20, MALE, "Belarus", "Minsk");
+        persons[11] = new Person("Andrey", 22, MALE, "Belarus", "Minsk");
+        persons[12] = new Person("Alexander", 26, MALE, "Belarus", "Minsk");
+        persons[13] = new Person("Vitaliy", 27, MALE, "Belarus", "Minsk");
+        persons[14] = new Person("Vlad", 24, MALE, "Belarus", "Minsk");
+        MilitaryOffice militaryOffice = new MilitaryOffice(new PersonRegistry(persons));
+        System.out.println(Arrays.toString(militaryOffice.getRecruitsByName("Alexander")));
+        System.out.println(militaryOffice.getRecruitsExactAge(18, 20) + "\n");
+        System.out.println(Arrays.toString(militaryOffice.getRecruitsInExactCity()) + "\n");
+        System.out.println(militaryOffice.getRecruits() + "\n");
+    }
 }
--- Lesson8/src/main/java/militaryOffice/MilitaryOffice.java
+++ Lesson8/src/main/java/militaryOffice/MilitaryOffice.java
@@ -1,5 +1,7 @@
 package militaryOffice;
 
+import static militaryOffice.Sex.MALE;
+
 public class MilitaryOffice {
     private PersonRegistry personRegistry;
 
@@ -17,16 +19,42 @@ public class MilitaryOffice {
         return count;
     }
 
+    private boolean checkRecruits(Person person) {
+        return person.getAge() >= 18 && person.getAge() <= 27 && person.getSex().equals(MALE);
+    }
+
+    public Person[] getRecruitsByName(String name) {
+        Person[] persons = personRegistry.getPersons();
+        Person[] personRecruits = new Person[persons.length];
+        for (int i = 0; i < persons.length; i++) {
+            Person person = persons[i];
+            if (checkRecruits(person) && person.getName().equalsIgnoreCase(name)) {
+                personRecruits[i] = person;
+            }
+        }
+        return personRecruits;
+    }
+
     public Person[] getRecruitsInExactCity() {
         Person[] persons = personRegistry.getPersons();
         Person[] personRecruits = new Person[persons.length];
         for (int i = 0; i < persons.length; i++) {
             Person person = persons[i];
-            if (checRecruits(person) && person.getName().equalsIgnoreCase(name)) {
+            if (checkRecruits(person) && person.getCity().equalsIgnoreCase("Minsk")) {
                 personRecruits[i] = person;
             }
         }
         return personRecruits;
+    }
+
+    public int getRecruitsExactAge(int from, int to) {
+        int count = 0;
+        for (Person persons : personRegistry.getPersons()) {
+            if (persons.getAge() >= from && persons.getAge() <= to && checkRecruits(persons)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
