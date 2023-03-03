1575ed422b8ef093dc09262935c99785f7cb5817
Nikolay <alda1995yurevich@gmail.com>
2023-03-03 20:16:53 +0300

    113

--- Lesson31Pattern/src/main/java/models/Head.java
+++ Lesson31Pattern/src/main/java/models/Head.java
@@ -1,12 +1,11 @@
 package models;
 
 public class Head implements CreateRobokopPartsImpl {
-    @Override
     public void getFunction(String info) {
         if (info == null) {
             System.out.println("у птички  нет яичек");
         } else {
-            System.out.println("есть яичко у орленка");
+              System.out.println("есть яичко у орленка");
         }
 
     }
